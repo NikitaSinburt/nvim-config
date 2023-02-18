2360283fd1c8102673f2c1fdc49954a2ad5de32d
NikitaSinburt <nikitasirotenko47@gmail.com>
2023-02-18 12:36:10 +0200

    fix(tree-sitter): finding refs with tree-sitter + git

--- init.lua
+++ init.lua
@@ -8,5 +8,11 @@ require("sinburt.plugins.lualine")
 require("sinburt.plugins.telescope")
 require("sinburt.plugins.nvim-cmp")
 require("sinburt.plugins.lsp.mason")
+-- error in one of those
 require("sinburt.plugins.lsp.lspsaga")
 require("sinburt.plugins.lsp.lspconfig")
+-------------------
+require("sinburt.plugins.lsp.null-ls")
+require("sinburt.plugins.autopairs")
+require("sinburt.plugins.treesitter")
+require("sinburt.plugins.gitsigns")
